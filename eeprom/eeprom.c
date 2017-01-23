6b5908a4d001ab633e5a696722f6dd6aad824abf
sunny.guo <sunny.guo@myirtech.com>
2017-01-23 15:23:00 +0800

    MYIRTECH: Add eeprom test application
    1. Add eeprom test application
       write and read string to eeprom specified address

---
+++ eeprom/eeprom.c
@@ -0,0 +1,141 @@
+/******************************************************************************
+* Copyright (C), 2016-2017, Sunny.Guo
+* FileName: tty.c 
+* Author: Sunny.Guo
+* Version: 1.0
+* Date: 2017年 01月 19日 星期四 10:12:50 CST
+* Description: can operation functions
+*
+* History:        
+*  <author>  	<time>   	<version >   	<desc>
+*  Sunny.Guo   	19/01/2017      1.0     	create this moudle  
+*                                                                          
+* Licensed under GPLv2 or later, see file LICENSE in this source tree.
+*******************************************************************************/
+#include "common.h"
+#include "eeprom.h"
+/******************************************************************************
+  Function:       eeprom_wp
+  Description:    set eeprom write protect
+  Input:          wp_enable       --  1: write protect enable; 0: write protect disable.
+                  
+  Output:          
+  Return:         int 	   		  --  status 0:success 
+  Others:         NONE
+*******************************************************************************/
+int 			eeprom_wp(int wp_enable)
+{
+	int ret = 0;
+	char cmdline[256] = { '\0' };
+	int  gpio = 103;
+
+	if(wp_enable == EEPROM_WP_ENABLE){
+		sprintf(cmdline,
+				"echo %d > /sys/class/gpio/unexport; echo %d > /sys/class/gpio/export; echo \"out\" > /sys/class/gpio/gpio%d/direction; echo 1 > /sys/class/gpio/gpio%d/value",
+				gpio, gpio, gpio, gpio);
+		}
+	else if(wp_enable == EEPROM_WP_DISABLE){
+		sprintf(cmdline,
+				"echo %d > /sys/class/gpio/unexport; echo %d > /sys/class/gpio/export; echo \"out\" > /sys/class/gpio/gpio%d/direction; echo 0 > /sys/class/gpio/gpio%d/value",
+				gpio, gpio, gpio, gpio);
+		}
+
+	ret = system(cmdline);
+	return WEXITSTATUS(ret);
+}
+
+
+int 			eeprom_init(char *dev)
+{
+	int fd = -1;
+	fd = open(dev,O_RDWR);
+	if(fd < 0) {
+		perror("open error");
+		return -1;
+	}
+	
+	ioctl(fd,I2C_TIMEOUT,1);
+	ioctl(fd,I2C_RETRIES,2);
+
+	return fd;
+}
+
+int 			eeprom_write(int fd, int addr, int start,  char * data, int len)
+{
+	int ret = 0;
+	struct i2c_rdwr_ioctl_data e2prom_data;
+	char buffer[EEPROM_BUFFER_SIZE] = {0};
+	e2prom_data.nmsgs=1;
+	e2prom_data.msgs=(struct i2c_msg*)malloc(e2prom_data.nmsgs*sizeof(struct i2c_msg));
+	if(!e2prom_data.msgs) {
+		perror("malloc error");
+		return -1;
+	}
+	
+	e2prom_data.nmsgs=1;
+	e2prom_data.msgs[0].len = len+2;
+	e2prom_data.msgs[0].addr = addr;
+	e2prom_data.msgs[0].flags = I2C_M_WR; //write
+	e2prom_data.msgs[0].buf= buffer;
+	
+	e2prom_data.msgs[0].buf[0]=(char)((start >>8)&0xff);// e2prom addr[15:8] 
+	e2prom_data.msgs[0].buf[1]=(char)((start)&0xff);//e2prom addr[7:0]
+
+	memcpy(&buffer[2], data, len);
+
+	ret=ioctl(fd,I2C_RDWR,(unsigned long)&e2prom_data);
+	if(ret<0) {
+		perror("ioctl error1");
+	}
+
+	return ret;
+}
+
+int 			eeprom_read(int fd, int addr, int start, char *data, int len)
+{
+	int ret = 0;
+	struct i2c_rdwr_ioctl_data e2prom_data;
+	char buffer[EEPROM_BUFFER_SIZE] = {0};
+
+	e2prom_data.nmsgs=2;
+	e2prom_data.msgs=(struct i2c_msg*)malloc(e2prom_data.nmsgs*sizeof(struct i2c_msg));
+	if(!e2prom_data.msgs) {
+		perror("malloc error");
+		return -1;
+	}
+
+//	dbg_printf("read address 0x%x at offset 0x%x with %d bytes \r\n", addr, start, len);
+	e2prom_data.nmsgs=2;
+	e2prom_data.msgs[0].len=2;
+	e2prom_data.msgs[0].addr=addr;
+	e2prom_data.msgs[0].flags=I2C_M_WR;						// write
+	e2prom_data.msgs[0].buf=buffer;
+	e2prom_data.msgs[0].buf[0]=(char)((start >>8)&0xff);	// e2prom addr[15:8] 
+	e2prom_data.msgs[0].buf[1]=(char)((start)&0xff);		//e2prom addr[7:0]
+	e2prom_data.msgs[1].len= len;
+	e2prom_data.msgs[1].addr=addr;
+	e2prom_data.msgs[1].flags=I2C_M_RD;						// read
+	e2prom_data.msgs[1].buf= data;
+
+	ret=ioctl(fd,I2C_RDWR,(unsigned long)&e2prom_data);
+	if(ret < 0 ) {
+		("ioctl error2");
+	}
+
+	return ret;
+}
+
+int 			parse_address(char *address_arg)
+{
+	long address;
+	char *end;
+	address = strtol(address_arg, &end, 0);
+	if (*end || !*address_arg) {
+		fprintf(stderr, "Error: Chip address is not a number!\n");
+		return -1;
+		}
+	
+	return address;
+}
+
+
---
+++ eeprom/eeprom.h
@@ -0,0 +1,72 @@
+/******************************************************************************
+* Copyright (C), 2016-2017, Sunny.Guo
+* FileName: eeprom.h 
+* Author: Sunny.Guo
+* Version: 1.0
+* Date: 2017年 01月 19日 星期四 10:12:50 CST
+* Description: eeprom definition         
+*
+* History:        
+*  <author>  	<time>   	<version >   	<desc>
+*  Sunny.Guo   	19/01/2017      1.0     	create this moudle  
+*                                                                          
+* Licensed under GPLv2 or later, see file LICENSE in this source tree.
+*******************************************************************************/
+#ifndef __TTY_H__
+#include <linux/ioctl.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+
+#include "common.h"
+
+#define EEPROM_I2C_ADDR			0x50
+#define	EEPROM_WP_ENABLE		1
+#define EEPROM_WP_DISABLE		0
+
+// gpio3_7 --  3*32+7 = 103
+#define EEPROM_WP_GPIO			103
+#define EEPROM_BUFFER_SIZE		256
+
+#define I2C_RETRIES 			0x0701
+#define I2C_TIMEOUT 			0x0702
+#define I2C_RDWR 				0x0707 
+
+#define I2C_M_TEN 				0x0010
+#define I2C_M_RD 				0x0001
+#define I2C_M_WR				0x0000
+
+
+struct i2c_msg
+{
+	unsigned short addr;
+	unsigned short flags;
+	unsigned short len;
+	unsigned char *buf;
+};
+
+struct i2c_rdwr_ioctl_data
+{
+	struct i2c_msg *msgs;
+	int nmsgs;
+};
+
+/******************************************************************************
+  Function:       eeprom_wp
+  Description:    set eeprom write protect
+  Input:          wp_enable       --  1: write protect enable; 0: write protect disable.
+                  
+  Output:          
+  Return:         int 	   		  --  status 0:success 
+  Others:         NONE
+*******************************************************************************/
+int 			eeprom_wp(int wp_enable);
+
+int 			eeprom_init(char * dev);
+
+int 			eeprom_write(int fd, int addr, int start,  char * data, int len);
+
+int 			eeprom_read(int fd, int addr, int start, char *data, int len);
+
+int 			parse_address(char *address_arg);
+
+#endif	 // __TTY_H__	
